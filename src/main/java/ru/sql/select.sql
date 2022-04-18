5f904006ee5c4079b2d75dd92c36df43aad007f0
Aleksandr Bratchin <alexbratchin@gmail.com>
2022-04-18 22:25:00 +0300

    closes #15 Задание по БД, часть 2 (Исправление замечаний) 1)рандомное заполнение ок, но было бы неплохо и пару осмысленных вопросов руками добавить по выборкам Добавил файл insert2.sql с осмысленными вопросами 2) вопросы на которые есть правильные ответы - лишний подзапрос, достаточно одного. например: where q.quest_id in (select quest_id from answer where correct_answer = true) или даже так: where exists (select null from answer an where an.quest_id = q.quest_id and an.correct_answer = true) Исправил 3)вопросы без правильных ответов - меняем на not exists Исправил 4)пакет с самым большим количеством вопросов - не тот запрос в скрипте Не оттуда скопировал. Исправил 5)в подсчете количества вопросов подзапрос не нужен, достаточно сделать два джойна и посчитать count(rq.quest_id) left join rounds r on r.package_id = p.package_id left join round_quest rq on rq.round_id = r.round_id Исправил

--- src/main/java/ru/sql/select.sql
+++ src/main/java/ru/sql/select.sql
@@ -6,50 +6,34 @@ select text_quest,
        answer.correct_answer--, rt.theme
 from quest q
          left join answer on q.quest_id = answer.quest_id
-where q.quest_id in (select a.quest_id
-                     from answer as a
-                     where a.quest_id in (select quest_id
-                                          from answer as a2
-                                          where correct_answer = true
-                                          group by quest_id)) --есть ответы
-order by q.level_quest
+where exists (select answer  from answer an where an.quest_id = q.quest_id and an.correct_answer = true)
+order by q.level_quest, q.quest_id;
 
 --вопросы для которых нет правельных ответов
 select text_quest,
        comment_quest,
        level_quest,
        answer.answer,
-       answer.correct_answer
+       answer.correct_answer--, rt.theme
 from quest q
          left join answer on q.quest_id = answer.quest_id
-where q.quest_id not in (select a.quest_id
-                         from answer as a
-                         where a.quest_id in
-                               (select quest_id from answer as a2 where correct_answer = true group by quest_id))
-order by q.level_quest
+where not exists (select answer  from answer an where an.quest_id = q.quest_id and an.correct_answer = true)
+order by q.level_quest, q.quest_id;
 
 --пакет с самым большим количеством вопросов
-select text_quest,
-       comment_quest,
-       level_quest,
-       answer.answer,
-       answer.correct_answer
-from quest q
-         left join answer on q.quest_id = answer.quest_id
-where q.quest_id not in (select a.quest_id
-                         from answer as a
-                         where a.quest_id in
-                               (select quest_id from answer as a2 where correct_answer = true group by quest_id))
-order by q.level_quest
+select p.name_package,
+       count(rq.quest_id) as kol
+from packages p
+         left join rounds r on r.package_id = p.package_id
+         left join  round_quest rq on rq.round_id = r.round_id
+group by p.name_package
+order by kol desc  limit 1
 
 --вывести количество вопросов в каждом пакете
 select p.name_package,
-       count(r.quest_id) as kol
+       count(rq.quest_id) as kol
 from packages p
-         left join (select rounds.round_id, rounds.package_id, rounds.name_round, rq.quest_id
-                    from rounds
-                             left join round_quest rq on rounds.round_id = rq.round_id
-                    order by round_id) r
-                   on r.package_id = p.package_id
+         left join rounds r on r.package_id = p.package_id
+         left join round_quest rq on rq.round_id = r.round_id
 group by p.name_package
 order by p.name_package, kol
